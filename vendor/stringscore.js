2bef71bf4351148b9e046c7aca577be9c850f96e
Kevin Sawicki <kevinsawicki@gmail.com>
2014-01-08 10:01:59 -0800

    :lipstick: Increment acronym bonus

--- vendor/stringscore.js
+++ vendor/stringscore.js
@@ -75,12 +75,12 @@ module.exports = function(string, abbreviation) {
       }
     }
     else {
-  // Acronym Bonus
-  // Weighing Logic: Typing the first character of an acronym is as if you
-  // preceded it with two perfect character matches.
-  if (string.charAt(index_in_string - 1) === ' ') {
-    character_score += 0.8; // * Math.min(index_in_string, 5); // Cap bonus at 0.4 * 5
-  }
+      // Acronym Bonus
+      // Weighing Logic: Typing the first character of an acronym is as if you
+      // preceded it with two perfect character matches.
+      if (string.charAt(index_in_string - 1) === ' ') {
+        character_score += 0.8; // * Math.min(index_in_string, 5); // Cap bonus at 0.4 * 5
+      }
     }
 
     // Left trim the already matched part of the string
